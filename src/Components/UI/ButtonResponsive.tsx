238af90bb373b5f5440ecb4d6904f7f0eabe1540
Coen de Waal <administratie@vonsa.nl>
2022-10-08 09:37:51 +0200

    feat: github and mail icons

--- src/Components/UI/ButtonResponsive.tsx
+++ src/Components/UI/ButtonResponsive.tsx
@@ -1,27 +1,31 @@
 export const ButtonResponsive = ({
   onClick,
   children,
+  icon,
 }: {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   children: React.ReactNode;
+  icon?: boolean;
 }) => {
   return (
     <button
       className="p-3 pt-2 pb-2 md:p-5 md:pt-3 md:pb-3 rounded-3xl border-red-400 border font-bold flex gap-2 text-red-400 hover:bg-red-400 hover:text-white ease-in-out duration-300"
       onClick={onClick}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="w-6 h-6 hidden md:block"
-      >
-        <path
-          fillRule="evenodd"
-          d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z"
-          clipRule="evenodd"
-        />
-      </svg>
+      {icon && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 hidden md:block"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z"
+            clipRule="evenodd"
+          />
+        </svg>
+      )}
       {children}
     </button>
   );
