45e9148ad64195b6281a083510fc587a41cf34f8
Coen de Waal <administratie@vonsa.nl>
2022-11-17 11:52:05 +0100

    plausible analytics integration

--- src/setup.tsx
+++ src/setup.tsx
@@ -6,33 +6,44 @@ import { routeConfig } from "./config/routes.config";
 import { Layout } from "./Components/UI/Layout";
 import LazyFallback from "./pages/LazyFallback";
 import { navigationItems } from "./config/navigation.config";
+import Analytics from "./Components/Analytics/Analytics";
+import ConditionalWrapper from "./Components/Hoc/ConditionalWrapper";
 
 export const createApp = () => (
   // <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      {routeConfig.map((route) => {
-        return (
-          <Route
-            key={route.path}
-            element={
-              <>
-                <Layout>
-                  <Navbar
-                    navigationItems={navigationItems}
-                    config={route.navbarConfig}
-                  />
-                  <React.Suspense fallback={<LazyFallback />}>
-                    <route.component />
-                  </React.Suspense>
-                </Layout>
-              </>
-            }
-            path={route.path}
-          />
-        );
-      })}
-    </Routes>
-  </BrowserRouter>
+  <ConditionalWrapper
+    condition={!!process.env.REACT_APP_ANALYTICS_DATA_DOMAIN}
+    wrapper={(children) => (
+      <Analytics dataDomain={process.env.REACT_APP_ANALYTICS_DATA_DOMAIN!}>
+        {children}
+      </Analytics>
+    )}
+  >
+    <BrowserRouter>
+      <Routes>
+        {routeConfig.map((route) => {
+          return (
+            <Route
+              key={route.path}
+              element={
+                <>
+                  <Layout>
+                    <Navbar
+                      navigationItems={navigationItems}
+                      config={route.navbarConfig}
+                    />
+                    <React.Suspense fallback={<LazyFallback />}>
+                      <route.component />
+                    </React.Suspense>
+                  </Layout>
+                </>
+              }
+              path={route.path}
+            />
+          );
+        })}
+      </Routes>
+    </BrowserRouter>
+  </ConditionalWrapper>
   // </React.StrictMode>
-)
+);
